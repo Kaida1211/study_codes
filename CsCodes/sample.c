a744bd9caa799dd700d0ea8b7a6d784d48118715
Kaida1211 <ka00125892@gmail.com>
2025-09-10 14:24:39 +0900

    Update sample.c

--- CsCodes/sample.c
+++ CsCodes/sample.c
@@ -1,1 +1,42 @@
+#include<stdio.h>
 
+int add(int n1, int n2)
+{
+  int add;
+  add = n1 + n2;
+}
+
+int diff(int add(), int n)
+{
+    int diff;
+    add = add() - n1;
+}
+
+int multi(int diff(), int n)
+{
+  int multi;
+  multi = diff() * n1;
+}
+
+int main(void){
+
+  int p;
+  int add_total,add1,add2;
+  int diff;
+  int multi;
+
+  puts("enter any numbers");
+  
+  add1 = scanf("%d"&p);
+  add2 = scanf("%d"&p);
+  diff = scanf("%d"&p);
+  multi = scanf("%d"&p);
+    
+  printf("足し算;%d\n",add(add1,add2);
+  printf("引き算;%d\n",diff(add(),diff);
+  printf("引き算;%d\n",multi(diff(),multi);
+  
+  int rsl;
+  
+  return 0;
+}
